07d6e62618c2b8dd353371b60b02f6ef53de7688
halloleo <git@halloleo.dot.hailmail.dot.net>
2012-10-21 16:39:04 +1100

    mod: shell histroy via CTRL-up/down (as by normal emacs)

--- user-lisp/leo-shell.el
+++ user-lisp/leo-shell.el
@@ -2,19 +2,22 @@
 ;; shell and comint stuff
 ;;
 (setq explicit-bash-args '("--noediting" "-i"))
+(setq explicit-fakecygpty-args '("bash" "--noediting"))
  
 ;; a few things to do on windows (cos default shell is "cmdproxy")
 (when (or (eq system-type 'windows-nt) (eq system-type 'cygwin))
-  (setq shell-file-name "bash")
+  ;;(setq shell-file-name "fakecygpty")
+  ;;(setq shell-file-name "c:/MinGW/msys/1.0/bin/bash.exe")
+  (setq shell-file-name "bash") ;; this should be the cygwin bash
   (setenv "SHELL" shell-file-name)
   (setq shell-completion-execonly nil)) ;; so that shellscript without .exe work
 
 (defun leo-shell-mode-hook ()
   "cursor keys get mapped"
-  (local-set-key '[C-up] (lookup-key global-map [up]))
-  (local-set-key '[C-down] (lookup-key global-map [down]))
-  (local-set-key '[up] 'comint-previous-input)
-  (local-set-key '[down] 'comint-next-input)
+  ;; (local-set-key '[C-up] (lookup-key global-map [up]))
+  ;; (local-set-key '[C-down] (lookup-key global-map [down]))
+  ;; (local-set-key '[up] 'comint-previous-input)
+  ;; (local-set-key '[down] 'comint-next-input)
   (local-set-key '[(shift tab)] 'comint-previous-matching-input-from-input))
 
 (add-hook 'shell-mode-hook 'leo-shell-mode-hook)
