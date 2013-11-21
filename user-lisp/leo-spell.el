2e2ae3c7e0ef9254417d941ac83ff135c841b8fc
halloleo <git@halloleo.dot.hailmail.dot.net>
2013-11-21 14:52:10 +1100

    [add:] synonym integration

--- user-lisp/leo-spell.el
+++ user-lisp/leo-spell.el
@@ -1,7 +1,10 @@
 ;;
-;; spelling
+;; spelling and natural language helpers
 ;;
 
+;;
+;; ispell
+;;
 (setq-default ispell-program-name "aspell")
 
 (defun leo-spell-switch-mode ()
@@ -25,3 +28,20 @@
        (define-key flyspell-mode-map [?\C-x (control ?\.)] 'leo-spell-switch-dictionary)))
 
 
+;;
+;; synonyms
+;;
+(setq synonyms-file
+      (cond ((eq system-type 'windows-nt)
+             "c:/cygwin/usr/local/share/lang/mthesaur.txt")
+            (t
+             "/usr/local/share/lang/mthesaur.txt")))
+(setq synonyms-cache-file
+      (cond ((eq system-type 'windows-nt)
+             "c:/cygwin/usr/local/var/synonyms")
+            (t
+             "/usr/local/var/cache/synonyms")))
+
+;; because i have a synoyms autoload file in site-start.d I shouldn't need this: (but god knows why i do!)
+(autoload 'synonyms "synonyms.el"
+    "Show synonyms that match a regular expression (e.g. a word or phrase)." t)
