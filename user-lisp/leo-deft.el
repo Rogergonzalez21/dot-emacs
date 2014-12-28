4358dadcf099d3688366d6863ac7067e36f12d5b
halloleo <git@halloleo.dot.hailmail.dot.net>
2014-12-28 12:02:31 +1100

    lots of changes

---
+++ user-lisp/leo-deft.el
@@ -0,0 +1,80 @@
+;;
+;; deft (should replace notes stuff)
+;;
+(require 'deft)
+
+(defcustom leo-deft-done-directory (expand-file-name "~/.deft/_done")
+  "Deft directory."
+  :type 'directory
+  :safe 'stringp
+  :group 'leos
+  :group 'deft)
+
+(setq deft-directory
+      (expand-file-name "~/Dropbox/notes/deft"))
+(setq leo-deft-done-directory
+      (expand-file-name "~/Dropbox/notes/deft/_done"))
+
+(defun leo-deft-switch-and-filter-clear ()
+  "switch to deft buffer and clear filter."
+  (interactive)
+  ;; just to make sure message is displayed if deft buffer has to be created
+  (leo-deft-initialise-if-needed) 
+  ;; go one in the normal way
+  (deft)
+  (deft-filter-clear))
+
+(defun leo-deft-exists ()
+  (let ((dft (get-buffer deft-buffer)))
+    (if (bufferp dft)
+        (with-current-buffer dft
+          (eq major-mode 'deft-mode)))))
+
+(defun leo-deft-initialise-if-needed ()
+  "Make sure a deft buffer exists."
+  (interactive)
+  (if (not (leo-deft-exists))
+      (with-current-buffer (get-buffer-create deft-buffer)        
+        (if (not (eq major-mode 'deft-mode))
+            (with-temp-message (format "Initialise %s buffer" deft-buffer)
+              (deft-mode))))))
+
+(defvar leo-deft-name-history nil
+  "History list of names of deft notes entered in the minibuffer.
+
+Maximum length of the history list is determined by the value
+of `history-length', which see.")
+
+(defun leo-deft-new-file-named (file)
+  "Create a new file named FILE (or interactively prompt for a filename).
+
+Like `deft-new-file-named', just make sure deft buffer is created."
+  (interactive 
+   (list
+    (read-string "New filename (without extension): " nil 'leo-deft-name-history "scratch")))
+  (leo-deft-initialise-if-needed)
+  (print file)
+  (deft-new-file-named file))
+
+(defun leo-deft-make-file-done ()
+  "Make the file represented by the widget at the point done.
+If the point is not on a file widget, do nothing. Does NOT prompt before
+proceeding.
+
+Note: \"Making\" a file done means moveing it to the directory 
+`leo-deft-done-directory'."
+  (interactive)
+  (let ((filename (widget-get (widget-at) :tag)))
+    (when filename
+      ;;(when (y-or-n-p
+      ;;     (format "Make file %s done?" (file-name-nondirectory filename)))
+        (rename-file filename leo-deft-done-directory 1)
+        (message (format "%s made done" (file-name-nondirectory filename)))
+        (delq filename deft-current-files)
+        (delq filename deft-all-files)
+        (deft-refresh))))
+
+(eval-after-load 'deft
+    '(progn
+       (define-key deft-mode-map (kbd "C-c C-d") 'leo-deft-make-file-done)))
+    
---
+++ user-lisp/leo-textmodes.el
@@ -0,0 +1,92 @@
+;;
+;; things for document modes
+;;
+(require `real-auto-save)
+
+;;
+;; text-mode
+;;
+;; (quail-define-package
+;;  "leo-german-prefix-dq" "German" "DX>" t
+;;  "German (Deutsch) input method with prefix modifiers
+;; Key translation rules are:
+;;  :A -> Ä ->   :O -> Ö   :U -> Ü   :s -> ß
+;; Customisation: \"\" -> \"
+;; " nil t nil nil nil nil nil nil nil nil t)
+
+;; (quail-define-rules
+;;  (":A" ?Ä)
+;;  (":O" ?Ö)
+;;  (":U" ?Ü)
+;;  (":a" ?ä)
+;;  (":o" ?ö)
+;;  (":u" ?ü)
+;;  (":s" ?ß)
+;; )
+
+(defun leo-text-mode-hook-func ()
+  ;;(set-input-method "german-prefix")
+  (column-number-mode 1)
+  (flyspell-mode 1)
+  ;; smart tab
+  (require 'smart-tab)
+  (smart-tab-mode 1))
+
+ 
+(add-hook 'text-mode-hook
+           'leo-text-mode-hook-func)
+
+(add-hook 'lisp-interaction-mode-hook
+           'leo-text-mode-hook-func)
+
+(add-hook 'help-mode-hook
+           '(lambda () 
+              (column-number-mode 1)))
+
+;;
+;; markdown
+;;
+(autoload 'markdown-mode "markdown-mode.el"
+  "Major mode for editing Markdown files" t)
+
+(add-to-list 'auto-mode-alist '("\\.Rmd\\'" . markdown-mode))
+
+(defun leo-markdown-snippet (&optional output-buffer-name)
+  "special function to provide html snippet output"
+  (setq output-buffer-name (markdown output-buffer-name))
+  (with-current-buffer output-buffer-name
+    (set-buffer output-buffer-name)
+    (goto-char (point-min))
+    (html-mode))
+  output-buffer-name)
+
+(defun leo-markdown-snippet-other-window (&optional output-buffer-name)
+  " Run `markdown' on current buffer and display in other window"
+  (interactive)
+  (display-buffer (leo-markdown-snippet output-buffer-name)))
+
+(defun leo-markdown-fontify-buffer-wiki-links-empty ()
+  "Empty replacement for `markdown-fontify-buffer-wiki-links` due to hanging bug."
+  (interactive))
+
+(eval-after-load 'markdown-mode
+    '(progn
+       (define-key markdown-mode-map "\C-c\C-cs" 
+         'leo-markdown-snippet-other-window)
+       (fset 'markdown-fontify-buffer-wiki-links 
+             'leo-markdown-fontify-buffer-wiki-links-empty)))
+       
+
+(defun leo-markdown-timestamp ()
+   (interactive)
+   (insert (format-time-string "%Y-%m-%d %H:%M:%S +0000")))
+
+;;
+;; special stuff for Day One doentry files
+;; Attention: They are in nxml mode, which is a PROG mode!!!
+;;
+(defun leo-set-dayone-files-modes ()
+  (nxml-mode))
+
+(add-to-list 'auto-mode-alist '("\\.doentry$" . leo-set-dayone-files-modes))
+
